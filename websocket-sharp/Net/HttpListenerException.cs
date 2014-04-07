bb44aa45c997075a437415872663cb9003763012
sta <sta.blockhead@gmail.com>
2014-04-07 20:54:22 +0900

    Refactored HttpListenerException.cs

--- websocket-sharp/Net/HttpListenerException.cs
+++ websocket-sharp/Net/HttpListenerException.cs
@@ -1,111 +1,127 @@
-//
-// HttpListenerException.cs
-//	Copied from System.Net.HttpListenerException.cs
-//
-// Author:
-//	Gonzalo Paniagua Javier (gonzalo@novell.com)
-//
-// Copyright (c) 2005 Novell, Inc. (http://www.novell.com)
-//
-// Permission is hereby granted, free of charge, to any person obtaining
-// a copy of this software and associated documentation files (the
-// "Software"), to deal in the Software without restriction, including
-// without limitation the rights to use, copy, modify, merge, publish,
-// distribute, sublicense, and/or sell copies of the Software, and to
-// permit persons to whom the Software is furnished to do so, subject to
-// the following conditions:
-// 
-// The above copyright notice and this permission notice shall be
-// included in all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-//
+#region License
+/*
+ * HttpListenerException.cs
+ *
+ * This code is derived from System.Net.HttpListenerException.cs of Mono
+ * (http://www.mono-project.com).
+ *
+ * The MIT License
+ *
+ * Copyright (c) 2005 Novell, Inc. (http://www.novell.com)
+ * Copyright (c) 2012-2014 sta.blockhead
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#endregion
+
+#region Authors
+/*
+ * Authors:
+ * - Gonzalo Paniagua Javier <gonzalo@novell.com>
+ */
+#endregion
 
 using System;
 using System.ComponentModel;
 using System.Runtime.Serialization;
 
-namespace WebSocketSharp.Net {
+namespace WebSocketSharp.Net
+{
+  /// <summary>
+  /// The exception that is thrown when an error occurs processing an HTTP request.
+  /// </summary>
+  [Serializable]
+  public class HttpListenerException : Win32Exception
+  {
+    #region Protected Constructors
 
-	/// <summary>
-	/// The exception that is thrown when an error occurs processing an HTTP request.
-	/// </summary>
-	[Serializable]
-	public class HttpListenerException : Win32Exception {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HttpListenerException"/> class
+    /// from the specified instances of the <see cref="SerializationInfo"/> and
+    /// <see cref="StreamingContext"/> classes.
+    /// </summary>
+    /// <param name="serializationInfo">
+    /// A <see cref="SerializationInfo"/> that contains the serialized object data.
+    /// </param>
+    /// <param name="streamingContext">
+    /// A <see cref="StreamingContext"/> that represents the source for the deserialization.
+    /// </param>
+    protected HttpListenerException (
+      SerializationInfo serializationInfo, StreamingContext streamingContext)
+      : base (serializationInfo, streamingContext)
+    {
+    }
 
-		#region Public Constructors
+    #endregion
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="HttpListenerException"/> class.
-		/// </summary>
-		public HttpListenerException ()
-		{
-		}
+    #region Public Constructors
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="HttpListenerException"/> class
-		/// with the specified <paramref name="errorCode"/>.
-		/// </summary>
-		/// <param name="errorCode">
-		/// An <see cref="int"/> that contains an error code.
-		/// </param>
-		public HttpListenerException (int errorCode) : base (errorCode)
-		{
-		}
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HttpListenerException"/> class.
+    /// </summary>
+    public HttpListenerException ()
+    {
+    }
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="HttpListenerException"/> class
-		/// with the specified <paramref name="errorCode"/> and <paramref name="message"/>.
-		/// </summary>
-		/// <param name="errorCode">
-		/// An <see cref="int"/> that contains an error code.
-		/// </param>
-		/// <param name="message">
-		/// A <see cref="string"/> that describes the error.
-		/// </param>
-		public HttpListenerException (int errorCode, string message) : base (errorCode, message)
-		{
-		}
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HttpListenerException"/> class
+    /// with the specified <paramref name="errorCode"/>.
+    /// </summary>
+    /// <param name="errorCode">
+    /// An <see cref="int"/> that identifies the error.
+    /// </param>
+    public HttpListenerException (int errorCode)
+      : base (errorCode)
+    {
+    }
 
-		#endregion
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HttpListenerException"/> class
+    /// with the specified <paramref name="errorCode"/> and <paramref name="message"/>.
+    /// </summary>
+    /// <param name="errorCode">
+    /// An <see cref="int"/> that identifies the error.
+    /// </param>
+    /// <param name="message">
+    /// A <see cref="string"/> that describes the error.
+    /// </param>
+    public HttpListenerException (int errorCode, string message)
+      : base (errorCode, message)
+    {
+    }
 
-		#region Protected Constructor
+    #endregion
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="HttpListenerException"/> class
-		/// from the specified <see cref="SerializationInfo"/> and <see cref="StreamingContext"/> classes.
-		/// </summary>
-		/// <param name="serializationInfo">
-		/// A <see cref="SerializationInfo"/> that contains the information required to deserialize
-		/// the new <see cref="HttpListenerException"/> object.
-		/// </param>
-		/// <param name="streamingContext">
-		/// A <see cref="StreamingContext"/>.
-		/// </param>
-		protected HttpListenerException (SerializationInfo serializationInfo, StreamingContext streamingContext) : base (serializationInfo, streamingContext)
-		{
-		}
+    #region Public Properties
 
-		#endregion
+    /// <summary>
+    /// Gets the value that identifies the error that occurred.
+    /// </summary>
+    /// <value>
+    /// An <see cref="int"/> that identifies the error.
+    /// </value>
+    public override int ErrorCode {
+      get {
+        return NativeErrorCode;
+      }
+    }
 
-		#region Property
-
-		/// <summary>
-		/// Gets a value that represents the error that occurred.
-		/// </summary>
-		/// <value>
-		/// An <see cref="int"/> that contains an error code.
-		/// </value>
-		public override int ErrorCode {
-			get { return base.ErrorCode; }
-		}
-
-		#endregion
-	}
+    #endregion
+  }
 }
