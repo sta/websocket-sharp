35b8848d383fffbdf3f91d55d09dd4e449e3f5b3
sta <sta.blockhead@gmail.com>
2014-07-05 17:01:24 +0900

    Refactored CloseEventArgs.cs

--- websocket-sharp/CloseEventArgs.cs
+++ websocket-sharp/CloseEventArgs.cs
@@ -32,13 +32,12 @@ using System.Text;
 namespace WebSocketSharp
 {
   /// <summary>
-  /// Contains the event data associated with a <see cref="WebSocket.OnClose"/>
-  /// event.
+  /// Contains the event data associated with a <see cref="WebSocket.OnClose"/> event.
   /// </summary>
   /// <remarks>
-  /// A <see cref="WebSocket.OnClose"/> event occurs when the WebSocket connection
-  /// has been closed. If you want to get the reason for closure, you access the
-  /// <see cref="Code"/> or <see cref="Reason"/> property.
+  /// A <see cref="WebSocket.OnClose"/> event occurs when the WebSocket connection has been closed.
+  /// If you would like to get the reason for the close, you should access the <see cref="Code"/> or
+  /// <see cref="Reason"/> property.
   /// </remarks>
   public class CloseEventArgs : EventArgs
   {
@@ -55,9 +54,14 @@ namespace WebSocketSharp
     internal CloseEventArgs (PayloadData payload)
     {
       var data = payload.ApplicationData;
-      _code = getCodeFrom (data);
-      _reason = getReasonFrom (data);
-      _clean = false;
+      var len = data.Length;
+      _code = len > 1
+              ? data.SubArray (0, 2).ToUInt16 (ByteOrder.Big)
+              : (ushort) CloseStatusCode.NoStatusCode;
+
+      _reason = len > 2
+                ? Encoding.UTF8.GetString (data.SubArray (2, len - 2))
+                : String.Empty;
     }
 
     #endregion
@@ -65,10 +69,10 @@ namespace WebSocketSharp
     #region Public Properties
 
     /// <summary>
-    /// Gets the status code for closure.
+    /// Gets the status code for the close.
     /// </summary>
     /// <value>
-    /// A <see cref="ushort"/> that indicates the status code for closure if any.
+    /// A <see cref="ushort"/> that represents the status code for the close if any.
     /// </value>
     public ushort Code {
       get {
@@ -77,10 +81,10 @@ namespace WebSocketSharp
     }
 
     /// <summary>
-    /// Gets the reason for closure.
+    /// Gets the reason for the close.
     /// </summary>
     /// <value>
-    /// A <see cref="string"/> that represents the reason for closure if any.
+    /// A <see cref="string"/> that represents the reason for the close if any.
     /// </value>
     public string Reason {
       get {
@@ -89,12 +93,10 @@ namespace WebSocketSharp
     }
 
     /// <summary>
-    /// Gets a value indicating whether the WebSocket connection has been closed
-    /// cleanly.
+    /// Gets a value indicating whether the WebSocket connection has been closed cleanly.
     /// </summary>
     /// <value>
-    /// <c>true</c> if the WebSocket connection has been closed cleanly;
-    /// otherwise, <c>false</c>.
+    /// <c>true</c> if the WebSocket connection has been closed cleanly; otherwise, <c>false</c>.
     /// </value>
     public bool WasClean {
       get {
@@ -104,25 +106,6 @@ namespace WebSocketSharp
       internal set {
         _clean = value;
       }
-    }
-
-    #endregion
-
-    #region Private Methods
-
-    private static ushort getCodeFrom (byte [] data)
-    {
-      return data.Length > 1
-             ? data.SubArray (0, 2).ToUInt16 (ByteOrder.Big)
-             : (ushort) CloseStatusCode.NoStatusCode;
-    }
-
-    private static string getReasonFrom (byte [] data)
-    {
-      var len = data.Length;
-      return len > 2
-             ? Encoding.UTF8.GetString (data.SubArray (2, len - 2))
-             : String.Empty;
     }
 
     #endregion
