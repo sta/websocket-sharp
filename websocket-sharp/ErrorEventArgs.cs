f641b9b611c3029eab80e3893e78c55723ea0418
sta <sta.blockhead@gmail.com>
2014-07-07 19:24:52 +0900

    Refactored ErrorEventArgs.cs

--- websocket-sharp/ErrorEventArgs.cs
+++ websocket-sharp/ErrorEventArgs.cs
@@ -4,8 +4,8 @@
  *
  * The MIT License
  *
- * Copyright (c) 2012-2013 sta.blockhead
- * 
+ * Copyright (c) 2012-2014 sta.blockhead
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
@@ -15,7 +15,7 @@
  *
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -35,15 +35,22 @@ namespace WebSocketSharp
   /// </summary>
   /// <remarks>
   /// A <see cref="WebSocket.OnError"/> event occurs when the <see cref="WebSocket"/> gets an error.
-  /// If you want to get the error message, you access the <see cref="ErrorEventArgs.Message"/> property.
+  /// If you would like to get the error message, you should access the <see cref="Message"/>
+  /// property.
   /// </remarks>
   public class ErrorEventArgs : EventArgs
   {
+    #region Private Fields
+
+    private string _message;
+
+    #endregion
+
     #region Internal Constructors
 
     internal ErrorEventArgs (string message)
     {
-      Message = message;
+      _message = message;
     }
 
     #endregion
@@ -54,9 +61,13 @@ namespace WebSocketSharp
     /// Gets the error message.
     /// </summary>
     /// <value>
-    /// A <see cref="string"/> that contains an error message.
+    /// A <see cref="string"/> that represents the error message.
     /// </value>
-    public string Message { get; private set; }
+    public string Message {
+      get {
+        return _message;
+      }
+    }
 
     #endregion
   }
