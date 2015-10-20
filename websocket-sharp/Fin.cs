41e37f750a3c6d1a922c95b975bf2fe0f9c1d640
sta <sta.blockhead@gmail.com>
2015-10-20 17:37:49 +0900

    [Modify] Polish it
    Add some xml doc comments.

--- websocket-sharp/Fin.cs
+++ websocket-sharp/Fin.cs
@@ -4,7 +4,7 @@
  *
  * The MIT License
  *
- * Copyright (c) 2012-2014 sta.blockhead
+ * Copyright (c) 2012-2015 sta.blockhead
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -30,9 +30,22 @@ using System;
 
 namespace WebSocketSharp
 {
+  /// <summary>
+  /// Contains the values that indicate whether a WebSocket frame is the final frame of a message.
+  /// </summary>
+  /// <remarks>
+  /// The values of this enumeration are defined in
+  /// <see href="http://tools.ietf.org/html/rfc6455#section-5.2">Section 5.2</see> of RFC 6455.
+  /// </remarks>
   internal enum Fin : byte
   {
+    /// <summary>
+    /// Equivalent to numeric value 0. Indicates more frames of a message follow.
+    /// </summary>
     More = 0x0,
+    /// <summary>
+    /// Equivalent to numeric value 1. Indicates the final frame of a message.
+    /// </summary>
     Final = 0x1
   }
 }
