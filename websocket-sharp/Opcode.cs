5fc3d52d1f2671d56f0a0a9ce6b92bcdc6dae270
sta <sta.blockhead@gmail.com>
2015-11-10 15:11:48 +0900

    [Modify] Polish it

--- websocket-sharp/Opcode.cs
+++ websocket-sharp/Opcode.cs
@@ -31,7 +31,7 @@ using System;
 namespace WebSocketSharp
 {
   /// <summary>
-  /// Contains the values of the opcode that indicate the type of a WebSocket frame.
+  /// Indicates the WebSocket frame type.
   /// </summary>
   /// <remarks>
   /// The values of this enumeration are defined in
@@ -40,27 +40,27 @@ namespace WebSocketSharp
   public enum Opcode : byte
   {
     /// <summary>
-    /// Equivalent to numeric value 0. Indicates a continuation frame.
+    /// Equivalent to numeric value 0. Indicates continuation frame.
     /// </summary>
     Cont = 0x0,
     /// <summary>
-    /// Equivalent to numeric value 1. Indicates a text frame.
+    /// Equivalent to numeric value 1. Indicates text frame.
     /// </summary>
     Text = 0x1,
     /// <summary>
-    /// Equivalent to numeric value 2. Indicates a binary frame.
+    /// Equivalent to numeric value 2. Indicates binary frame.
     /// </summary>
     Binary = 0x2,
     /// <summary>
-    /// Equivalent to numeric value 8. Indicates a connection close frame.
+    /// Equivalent to numeric value 8. Indicates connection close frame.
     /// </summary>
     Close = 0x8,
     /// <summary>
-    /// Equivalent to numeric value 9. Indicates a ping frame.
+    /// Equivalent to numeric value 9. Indicates ping frame.
     /// </summary>
     Ping = 0x9,
     /// <summary>
-    /// Equivalent to numeric value 10. Indicates a pong frame.
+    /// Equivalent to numeric value 10. Indicates pong frame.
     /// </summary>
     Pong = 0xa
   }
