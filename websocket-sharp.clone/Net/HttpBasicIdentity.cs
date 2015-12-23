f9abf9bf86003d5429008025b65ffe1b0e5a3500
danielmarbach <daniel.marbach@openplace.net>
2015-12-23 22:50:50 +0100

    HttpBasicIdentity

--- websocket-sharp.clone/Net/HttpBasicIdentity.cs
+++ websocket-sharp.clone/Net/HttpBasicIdentity.cs
@@ -47,38 +47,18 @@ namespace WebSocketSharp.Net
 	/// </summary>
 	internal class HttpBasicIdentity : GenericIdentity
 	{
-		#region Private Fields
-
-		private string _password;
-
-		#endregion
-
-		#region internal Constructors
-
-		internal HttpBasicIdentity(string username, string password)
+	    internal HttpBasicIdentity(string username, string password)
 			: base(username, "Basic")
 		{
-			_password = password;
+			Password = password;
 		}
 
-		#endregion
-
-		#region Public Properties
-
-		/// <summary>
+	    /// <summary>
 		/// Gets the password from the HTTP Basic authentication credentials.
 		/// </summary>
 		/// <value>
 		/// A <see cref="string"/> that represents the password.
 		/// </value>
-		public virtual string Password
-		{
-			get
-			{
-				return _password;
-			}
-		}
-
-		#endregion
+		public virtual string Password { get; }
 	}
 }
