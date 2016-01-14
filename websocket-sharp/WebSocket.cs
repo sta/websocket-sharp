43fb6368bb28ca4631c5d1e2096e900c53bd9dc2
sta <sta.blockhead@gmail.com>
2016-01-14 16:02:18 +0900

    [Modify] Replace it

--- websocket-sharp/WebSocket.cs
+++ websocket-sharp/WebSocket.cs
@@ -1852,7 +1852,8 @@ namespace WebSocketSharp
         }
       }
       catch (Exception ex) {
-        processException (ex, "An exception has occurred while accepting.");
+        _logger.Fatal (ex.ToString ());
+        fatal ("An exception has occurred while accepting.", ex);
       }
     }
 
