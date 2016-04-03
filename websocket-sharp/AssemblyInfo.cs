863700c49fe2f90955da70256a3c650f9ce03c5e
Gilberto Polzoni <g.polzoni@loccioni.com>
2016-04-03 15:44:16 +0200

    Added missing attribute for nuget spec

--- websocket-sharp/AssemblyInfo.cs
+++ websocket-sharp/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyTitle("websocket-sharp")]
 [assembly: AssemblyDescription("A C# implementation of the WebSocket protocol client and server")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("sta.blockhead")]
 [assembly: AssemblyProduct("websocket-sharp.dll")]
 [assembly: AssemblyCopyright("sta.blockhead")]
 [assembly: AssemblyTrademark("")]
