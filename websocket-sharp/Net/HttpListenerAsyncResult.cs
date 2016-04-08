b25343f7253a1fb9b28a6768df01bf7652b65172
sta <sta.blockhead@gmail.com>
2016-04-08 15:48:30 +0900

    [Fix] Set system error code 995

--- websocket-sharp/Net/HttpListenerAsyncResult.cs
+++ websocket-sharp/Net/HttpListenerAsyncResult.cs
@@ -162,7 +162,7 @@ namespace WebSocketSharp.Net
     internal void Complete (Exception exception)
     {
       _exception = _inGet && (exception is ObjectDisposedException)
-                   ? new HttpListenerException (500, "Listener closed.")
+                   ? new HttpListenerException (995, "Listener closed.")
                    : exception;
 
       lock (_sync)
