e09a8c2f45c49c5835b4f6b13e1268f2a2402b0b
sta <sta.blockhead@gmail.com>
2016-08-30 14:35:18 +0900

    [Modify] Replace it

--- websocket-sharp/WebSocket.cs
+++ websocket-sharp/WebSocket.cs
@@ -2531,10 +2531,15 @@ namespace WebSocketSharp
     /// </param>
     public void CloseAsync (ushort code, string reason)
     {
-      var msg = _readyState.CheckIfAvailable (true, true, false, false) ??
-                CheckCloseParameters (code, reason, _client);
+      string msg;
+      if (!checkIfAvailable (true, true, false, false, out msg)) {
+        _logger.Error (msg);
+        error ("An error has occurred in closing the connection.", null);
 
-      if (msg != null) {
+        return;
+      }
+
+      if (!CheckParametersForClose (code, reason, _client, out msg)) {
         _logger.Error (msg);
         error ("An error has occurred in closing the connection.", null);
 
