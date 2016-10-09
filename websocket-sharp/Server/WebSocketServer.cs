25c8d9d1ec0557cdb5b933cf7c43b6e10574c53d
sta <sta.blockhead@gmail.com>
2016-10-09 16:25:50 +0900

    [Modify] Edit it

--- websocket-sharp/Server/WebSocketServer.cs
+++ websocket-sharp/Server/WebSocketServer.cs
@@ -289,10 +289,11 @@ namespace WebSocketSharp.Server
     #region Public Properties
 
     /// <summary>
-    /// Gets the local IP address of the server.
+    /// Gets the IP address of the server.
     /// </summary>
     /// <value>
-    /// A <see cref="System.Net.IPAddress"/> that represents the local IP address of the server.
+    /// A <see cref="System.Net.IPAddress"/> that represents
+    /// the IP address of the server.
     /// </value>
     public System.Net.IPAddress Address {
       get {
