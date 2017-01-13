fb62b1e60fcdac840a5d27c755e7b6c3f0ebbf84
sta <sta.blockhead@gmail.com>
2017-01-13 16:20:48 +0900

    [Modify] Edit it

--- websocket-sharp/Server/WebSocketServiceHost.cs
+++ websocket-sharp/Server/WebSocketServiceHost.cs
@@ -39,11 +39,12 @@ using WebSocketSharp.Net.WebSockets;
 namespace WebSocketSharp.Server
 {
   /// <summary>
-  /// Exposes the methods and properties used to access the information in a WebSocket service
-  /// provided by the <see cref="WebSocketServer"/> or <see cref="HttpServer"/>.
+  /// Exposes the methods and properties used to access the information in
+  /// a WebSocket service provided by the <see cref="WebSocketServer"/> or
+  /// <see cref="HttpServer"/>.
   /// </summary>
   /// <remarks>
-  /// The WebSocketServiceHost class is an abstract class.
+  /// This class is an abstract class.
   /// </remarks>
   public abstract class WebSocketServiceHost
   {
