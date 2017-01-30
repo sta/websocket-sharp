93fb098bb15d91ac6c2fb0dd5a612592d0cebfd8
sta <sta.blockhead@gmail.com>
2017-01-30 16:21:00 +0900

    [Modify] Polish it

--- websocket-sharp/Server/WebSocketServer.cs
+++ websocket-sharp/Server/WebSocketServer.cs
@@ -285,11 +285,12 @@ namespace WebSocketSharp.Server
         throw new ArgumentNullException ("address");
 
       if (!address.IsLocal ())
-        throw new ArgumentException ("Not a local IP address: " + address, "address");
+        throw new ArgumentException ("Not a local IP address.", "address");
 
-      if (!port.IsPortNumber ())
-        throw new ArgumentOutOfRangeException (
-          "port", "Not between 1 and 65535 inclusive: " + port);
+      if (!port.IsPortNumber ()) {
+        var msg = "It is less than 1 or greater than 65535.";
+        throw new ArgumentOutOfRangeException ("port", msg);
+      }
 
       init (null, address, port, secure);
     }
