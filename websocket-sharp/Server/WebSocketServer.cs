47d3b96f00ffe01840fa77d61b966e4753a05ac8
sta <sta.blockhead@gmail.com>
2017-01-30 16:42:27 +0900

    [Modify] Edit it

--- websocket-sharp/Server/WebSocketServer.cs
+++ websocket-sharp/Server/WebSocketServer.cs
@@ -224,15 +224,16 @@ namespace WebSocketSharp.Server
     /// </summary>
     /// <remarks>
     ///   <para>
-    ///   An instance initialized by this constructor listens for the incoming connection requests
-    ///   on <paramref name="address"/> and <paramref name="port"/>.
+    ///   The new instance listens for the incoming handshake requests on
+    ///   <paramref name="address"/> and <paramref name="port"/>.
     ///   </para>
     ///   <para>
-    ///   If <paramref name="port"/> is 443, that instance provides a secure connection.
+    ///   It provides secure connections if <paramref name="port"/> is 443.
     ///   </para>
     /// </remarks>
     /// <param name="address">
-    /// A <see cref="System.Net.IPAddress"/> that represents the local IP address of the server.
+    /// A <see cref="System.Net.IPAddress"/> that represents the local IP address
+    /// for the server.
     /// </param>
     /// <param name="port">
     /// An <see cref="int"/> that represents the port number on which to listen.
@@ -241,10 +242,10 @@ namespace WebSocketSharp.Server
     /// <paramref name="address"/> is <see langword="null"/>.
     /// </exception>
     /// <exception cref="ArgumentException">
-    /// <paramref name="address"/> isn't a local IP address.
+    /// <paramref name="address"/> is not a local IP address.
     /// </exception>
     /// <exception cref="ArgumentOutOfRangeException">
-    /// <paramref name="port"/> isn't between 1 and 65535 inclusive.
+    /// <paramref name="port"/> is less than 1 or greater than 65535.
     /// </exception>
     public WebSocketServer (System.Net.IPAddress address, int port)
       : this (address, port, port == 443)
