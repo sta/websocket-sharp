3a759e12ea833d77710dead4abbc583a30901de3
sta <sta.blockhead@gmail.com>
2017-04-30 15:12:09 +0900

    [Modify] Edit it

--- websocket-sharp/Net/NetworkCredential.cs
+++ websocket-sharp/Net/NetworkCredential.cs
@@ -129,6 +129,10 @@ namespace WebSocketSharp.Net
     /// <summary>
     /// Gets the domain associated with the credentials.
     /// </summary>
+    /// <remarks>
+    /// This property returns an empty string if the domain was
+    /// initialized with <see langword="null"/>.
+    /// </remarks>
     /// <value>
     /// A <see cref="string"/> that represents the domain name
     /// to which the username belongs.
