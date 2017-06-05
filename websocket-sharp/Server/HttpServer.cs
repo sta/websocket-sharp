6185f6753526189db7c056cc2a61174c6bc58085
sta <sta.blockhead@gmail.com>
2017-06-05 15:20:19 +0900

    [Modify] Replace it

--- websocket-sharp/Server/HttpServer.cs
+++ websocket-sharp/Server/HttpServer.cs
@@ -437,13 +437,20 @@ namespace WebSocketSharp.Server
       }
 
       set {
-        var msg = _state.CheckIfAvailable (true, false, false);
-        if (msg != null) {
-          _log.Error (msg);
+        string msg;
+        if (!canSet (out msg)) {
+          _log.Warn (msg);
           return;
         }
 
-        _listener.Realm = value;
+        lock (_sync) {
+          if (!canSet (out msg)) {
+            _log.Warn (msg);
+            return;
+          }
+
+          _listener.Realm = value;
+        }
       }
     }
 
