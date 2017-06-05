30be47d7de92c6335da3a57c43df593470f36160
sta <sta.blockhead@gmail.com>
2017-06-05 15:16:38 +0900

    [Modify] Edit it

--- websocket-sharp/Server/HttpServer.cs
+++ websocket-sharp/Server/HttpServer.cs
@@ -353,11 +353,20 @@ namespace WebSocketSharp.Server
 
     /// <summary>
     /// Gets or sets a value indicating whether the server cleans up
-    /// the inactive sessions in the WebSocket services periodically.
+    /// the inactive sessions periodically.
     /// </summary>
+    /// <remarks>
+    /// The set operation does nothing if the server has already
+    /// started or it is shutting down.
+    /// </remarks>
     /// <value>
-    /// <c>true</c> if the server cleans up the inactive sessions every 60 seconds;
-    /// otherwise, <c>false</c>. The default value is <c>true</c>.
+    ///   <para>
+    ///   <c>true</c> if the server cleans up the inactive sessions
+    ///   every 60 seconds; otherwise, <c>false</c>.
+    ///   </para>
+    ///   <para>
+    ///   The default value is <c>true</c>.
+    ///   </para>
     /// </value>
     public bool KeepClean {
       get {
