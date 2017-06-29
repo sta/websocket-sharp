daa6b95227195e2597fb33d561cf07863191d6b3
sta <sta.blockhead@gmail.com>
2017-06-29 15:43:40 +0900

    [Modify] Polish it

--- websocket-sharp/Server/HttpRequestEventArgs.cs
+++ websocket-sharp/Server/HttpRequestEventArgs.cs
@@ -193,17 +193,8 @@ namespace WebSocketSharp.Server
       if (path.Length == 0)
         throw new ArgumentException ("An empty string.", "path");
 
-      if (path.IndexOf (':') > -1)
-        throw new ArgumentException ("It contains ':'.", "path");
-
       if (path.IndexOf ("..") > -1)
         throw new ArgumentException ("It contains '..'.", "path");
-
-      if (path.IndexOf ("//") > -1)
-        throw new ArgumentException ("It contains '//'.", "path");
-
-      if (path.IndexOf ("\\\\") > -1)
-        throw new ArgumentException ("It contains '\\\\'.", "path");
 
       byte[] contents;
       tryReadFile (createFilePath (path), out contents);
