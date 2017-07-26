5a91fbd3184df66154701fc25587afdd7784a0c9
sta <sta.blockhead@gmail.com>
2017-07-26 15:16:27 +0900

    [Modify] Throw exceptions

--- websocket-sharp/WebSocket.cs
+++ websocket-sharp/WebSocket.cs
@@ -2652,12 +2652,35 @@ namespace WebSocketSharp
     /// </param>
     public void Close (CloseStatusCode code, string reason)
     {
-      string msg;
-      if (!CheckParametersForClose (code, reason, _client, out msg)) {
-        _logger.Error (msg);
-        error ("An error has occurred in closing the connection.", null);
+      if (_client && code == CloseStatusCode.ServerError) {
+        var msg = "ServerError cannot be used.";
+        throw new ArgumentException (msg, "code");
+      }
 
+      if (!_client && code == CloseStatusCode.MandatoryExtension) {
+        var msg = "MandatoryExtension cannot be used.";
+        throw new ArgumentException (msg, "code");
+      }
+
+      if (reason.IsNullOrEmpty ()) {
+        close ((ushort) code, String.Empty);
         return;
+      }
+
+      if (code == CloseStatusCode.NoStatus) {
+        var msg = "NoStatus cannot be used.";
+        throw new ArgumentException (msg, "code");
+      }
+
+      byte[] bytes;
+      if (!reason.TryGetUTF8EncodedBytes (out bytes)) {
+        var msg = "It could not be UTF-8-encoded.";
+        throw new ArgumentException (msg, "reason");
+      }
+
+      if (bytes.Length > 123) {
+        var msg = "Its size is greater than 123 bytes.";
+        throw new ArgumentOutOfRangeException ("reason", msg);
       }
 
       close ((ushort) code, reason);
