1ac454bd34224e14de74104555a867e34a6a0ca9
sta <sta.blockhead@gmail.com>
2017-08-04 15:56:45 +0900

    [Modify] Remove it

--- websocket-sharp/WebSocket.cs
+++ websocket-sharp/WebSocket.cs
@@ -2074,11 +2074,6 @@ namespace WebSocketSharp
 
     #region Internal Methods
 
-    internal static string CheckSendParameter (byte[] data)
-    {
-      return data == null ? "'data' is null." : null;
-    }
-
     internal static string CheckSendParameter (string data)
     {
       return data == null ? "'data' is null." : null;
