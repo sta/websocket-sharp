c2c9cafe850b625666924eddfe44b21d185e2cc9
nSwann09 <nSwann09@users.noreply.github.com>
2017-09-12 21:55:34 +0100

    IsPortNumber returns true for port 0

--- websocket-sharp/Ext.cs
+++ websocket-sharp/Ext.cs
@@ -506,7 +506,7 @@ namespace WebSocketSharp
 
     internal static bool IsPortNumber (this int value)
     {
-      return value > 0 && value < 65536;
+      return value > -1 && value < 65536;
     }
 
     internal static bool IsReserved (this ushort code)
