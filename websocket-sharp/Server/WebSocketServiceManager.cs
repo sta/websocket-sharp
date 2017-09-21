0a3ea71729fbb6423bd2791a069437ae2db45fa4
sta <sta.blockhead@gmail.com>
2017-09-21 19:39:10 +0900

    [Modify] Edit it

--- websocket-sharp/Server/WebSocketServiceManager.cs
+++ websocket-sharp/Server/WebSocketServiceManager.cs
@@ -251,11 +251,19 @@ namespace WebSocketSharp.Server
     }
 
     /// <summary>
-    /// Gets the wait time for the response to the WebSocket Ping or Close.
+    /// Gets or sets the time to wait for the response to the WebSocket Ping or
+    /// Close.
     /// </summary>
+    /// <remarks>
+    /// The set operation does nothing if the server has already started or
+    /// it is shutting down.
+    /// </remarks>
     /// <value>
-    /// A <see cref="TimeSpan"/> that represents the wait time for the response.
+    /// A <see cref="TimeSpan"/> to wait for the response.
     /// </value>
+    /// <exception cref="ArgumentOutOfRangeException">
+    /// The value specified for a set operation is zero or less.
+    /// </exception>
     public TimeSpan WaitTime {
       get {
         return _waitTime;
