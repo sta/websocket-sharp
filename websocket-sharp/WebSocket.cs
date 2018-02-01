ca0e04c1a499fa35557bd221c73fdf47b9d1bac7
Vive-VR-PC <andrey.kiselev@oru.se>
2018-02-01 09:46:15 +0100

    For some use cases, IsConnected method needs to be public.

--- websocket-sharp/WebSocket.cs
+++ websocket-sharp/WebSocket.cs
@@ -320,12 +320,6 @@ namespace WebSocketSharp
       }
     }
 
-    internal bool IsConnected {
-      get {
-        return _readyState == WebSocketState.Open || _readyState == WebSocketState.Closing;
-      }
-    }
-
     #endregion
 
     #region Public Properties
@@ -768,6 +762,12 @@ namespace WebSocketSharp
         }
       }
     }
+
+	public bool IsConnected {
+		get {
+			return _readyState == WebSocketState.Open || _readyState == WebSocketState.Closing;
+		}
+	}
 
     #endregion
 
