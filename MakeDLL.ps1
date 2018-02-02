431ec3aa4ab363eee5d4094657c05324ab3e6ffc
Nakata Nobuyuki <nobu.nakata@gmail.com>
2018-02-02 16:58:30 +0900

    DLL 自動生成スクリプト

---
+++ MakeDLL.ps1
@@ -0,0 +1,12 @@
+﻿param ([string]$output = ".")
+
+sal devenv "C:\Program Files (x86)\Microsoft Visual Studio\2017\Professional\Common7\IDE\devenv"
+
+$dll_folder = "websocket-sharp\bin\Release"
+$dll_name = "websocket-sharp.dll"
+
+cd $PSScriptRoot
+devenv websocket-sharp.sln /build Release /project websocket-sharp\websocket-sharp.csproj /projectconfig Release
+
+
+Copy-Item ".\$dll_folder\$dll_name" $dll_name
