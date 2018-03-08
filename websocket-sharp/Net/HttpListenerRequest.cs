1692b8cb7fafa5c9455a083ea4833369f655209e
sta <sta.blockhead@gmail.com>
2018-03-08 19:49:17 +0900

    [Modify] Edit it

--- websocket-sharp/Net/HttpListenerRequest.cs
+++ websocket-sharp/Net/HttpListenerRequest.cs
@@ -890,11 +890,11 @@ namespace WebSocketSharp.Net
     }
 
     /// <summary>
-    /// Returns a <see cref="string"/> that represents
-    /// the current <see cref="HttpListenerRequest"/>.
+    /// Returns a string that represents the current instance.
     /// </summary>
     /// <returns>
-    /// A <see cref="string"/> that represents the current <see cref="HttpListenerRequest"/>.
+    /// A <see cref="string"/> that contains the request line and headers
+    /// included in the request.
     /// </returns>
     public override string ToString ()
     {
