5c60c92f0740a170b4323437a7c1f1f2fba4a99f
sta <sta.blockhead@gmail.com>
2018-05-10 19:45:02 +0900

    [Modify] Edit it

--- websocket-sharp/Server/WebSocketBehavior.cs
+++ websocket-sharp/Server/WebSocketBehavior.cs
@@ -422,7 +422,7 @@ namespace WebSocketSharp.Server
     }
 
     /// <summary>
-    /// Called when the WebSocket instance used in a session gets an error.
+    /// Called when the WebSocket instance for a session gets an error.
     /// </summary>
     /// <param name="e">
     /// A <see cref="ErrorEventArgs"/> that represents the event data passed
