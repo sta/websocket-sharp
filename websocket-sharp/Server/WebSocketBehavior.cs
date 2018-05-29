904ecf21c4f6b0b0243dfbf708961af23702f7a8
sta <sta.blockhead@gmail.com>
2018-05-29 19:30:44 +0900

    [Modify] Edit it

--- websocket-sharp/Server/WebSocketBehavior.cs
+++ websocket-sharp/Server/WebSocketBehavior.cs
@@ -536,8 +536,8 @@ namespace WebSocketSharp.Server
     }
 
     /// <summary>
-    /// Sends the specified data to a client asynchronously using the WebSocket
-    /// connection.
+    /// Sends the specified data to a client asynchronously using
+    /// the WebSocket connection.
     /// </summary>
     /// <remarks>
     /// This method does not wait for the send to be complete.
