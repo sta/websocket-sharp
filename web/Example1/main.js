8e7a32173fbe31cfc04a5c99638ebb8569f4f9de
Dimitri Podborski <dimitri.podborski@hhi.fraunhofer.de>
2019-07-16 16:07:19 +0200

    send message to unity WSServer and get response based on it

--- web/Example1/main.js
+++ web/Example1/main.js
@@ -4,10 +4,20 @@ $(document).ready(function () {
     const connection = new WebSocket("ws://127.0.0.1:55555/Laputa");
     connection.onopen = () => {
         console.log("opened");
+        connection.send('BALUS');
     }
 
     connection.onerror = error => {
         console.log("WebSocket error");
         console.log(error);
+    }
+
+    connection.onclose = e => {
+        console.log("connection closes");
+        console.log(e);
+    }
+
+    connection.onmessage = (e) => {
+        console.log(e.data);
     }
 });
