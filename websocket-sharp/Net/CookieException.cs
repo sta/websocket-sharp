64f549d3d47f52397fb2198cd933910e1b5c3c22
Dimitri Podborski <dimitri.podborski@hhi.fraunhofer.de>
2019-07-16 10:39:11 +0200

    fix compiling bug of CookieException

--- websocket-sharp/Net/CookieException.cs
+++ websocket-sharp/Net/CookieException.cs
@@ -46,7 +46,7 @@ namespace WebSocketSharp.Net
   /// The exception that is thrown when a <see cref="Cookie"/> gets an error.
   /// </summary>
   [Serializable]
-  public class CookieException : FormatException
+  public class CookieException : FormatException, ISerializable
   {
     #region Internal Constructors
 
