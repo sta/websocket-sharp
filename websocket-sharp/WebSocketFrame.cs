2dccdc7b68496f2aa5df5ef73a0b204a86cb8541
sta <sta.blockhead@gmail.com>
2019-08-23 21:42:50 +0900

    [Modify] Polish it

--- websocket-sharp/WebSocketFrame.cs
+++ websocket-sharp/WebSocketFrame.cs
@@ -664,7 +664,7 @@ Extended Payload Length: {7}
       }
 
       if (len > PayloadData.MaxLength) {
-        var msg = "A frame has too long payload data length.";
+        var msg = "A frame has too long payload length.";
         throw new WebSocketException (CloseStatusCode.TooBig, msg);
       }
 
