e147aff46a9a660971a949bce924ae50fab4eafd
sta <sta.blockhead@gmail.com>
2019-09-11 22:05:41 +0900

    [Modify] Edit it

--- websocket-sharp/Ext.cs
+++ websocket-sharp/Ext.cs
@@ -1802,13 +1802,13 @@ namespace WebSocketSharp
     }
 
     /// <summary>
-    /// Executes the specified <see cref="Action"/> delegate <paramref name="n"/> times.
+    /// Executes the specified delegate the specified times.
     /// </summary>
     /// <param name="n">
-    /// A <see cref="long"/> is the number of times to execute.
+    /// A <see cref="long"/> that specifies the number of times to execute.
     /// </param>
     /// <param name="action">
-    /// An <see cref="Action"/> delegate that references the method(s) to execute.
+    /// An <see cref="Action"/> delegate to execute.
     /// </param>
     public static void Times (this long n, Action action)
     {
