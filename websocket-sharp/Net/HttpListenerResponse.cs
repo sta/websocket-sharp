0be5376ff6a6e7e7c9dd73f6146b26e3ca4950c2
sta <sta.blockhead@gmail.com>
2019-11-13 20:39:20 +0900

    [Modify] Edit it

--- websocket-sharp/Net/HttpListenerResponse.cs
+++ websocket-sharp/Net/HttpListenerResponse.cs
@@ -227,10 +227,11 @@ namespace WebSocketSharp.Net
     }
 
     /// <summary>
-    /// Gets or sets the cookies sent with the response.
+    /// Gets or sets the collection of cookies sent with the response.
     /// </summary>
     /// <value>
-    /// A <see cref="CookieCollection"/> that contains the cookies sent with the response.
+    /// A <see cref="CookieCollection"/> that contains the cookies sent with
+    /// the response.
     /// </value>
     public CookieCollection Cookies {
       get {
