813455317c46eb86709cb8a66a05cc654489259f
sta <sta.blockhead@gmail.com>
2019-12-18 22:08:43 +0900

    [Modify] Edit it

--- websocket-sharp/Net/HttpListenerResponse.cs
+++ websocket-sharp/Net/HttpListenerResponse.cs
@@ -330,6 +330,9 @@ namespace WebSocketSharp.Net
     /// <exception cref="ArgumentException">
     /// The value specified for a set operation is an empty string.
     /// </exception>
+    /// <exception cref="InvalidOperationException">
+    /// The response is already being sent.
+    /// </exception>
     /// <exception cref="ObjectDisposedException">
     /// This instance is closed.
     /// </exception>
