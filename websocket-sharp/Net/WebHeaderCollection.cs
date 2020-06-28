a4f64b2f1d3e7578a6f55d1a9f9f7ac2ec14ac2e
sta <sta.blockhead@gmail.com>
2020-06-28 21:43:19 +0900

    [Modify] Edit it

--- websocket-sharp/Net/WebHeaderCollection.cs
+++ websocket-sharp/Net/WebHeaderCollection.cs
@@ -1413,7 +1413,8 @@ namespace WebSocketSharp.Net
     /// Gets the enumerator used to iterate through the collection.
     /// </summary>
     /// <returns>
-    /// An <see cref="IEnumerator"/> instance used to iterate through the collection.
+    /// An <see cref="IEnumerator"/> instance used to iterate through
+    /// the collection.
     /// </returns>
     public override IEnumerator GetEnumerator ()
     {
