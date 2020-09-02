37e79ef5cb40abfbcb5022584462338b749c159c
sta <sta.blockhead@gmail.com>
2020-09-02 22:05:58 +0900

    [Modify] Edit it

--- websocket-sharp/Net/HttpListenerPrefixCollection.cs
+++ websocket-sharp/Net/HttpListenerPrefixCollection.cs
@@ -225,8 +225,8 @@ namespace WebSocketSharp.Net
     /// <paramref name="array"/> is not enough to copy to.
     /// </exception>
     /// <exception cref="ObjectDisposedException">
-    /// The <see cref="HttpListener"/> instance associated with
-    /// this collection is closed.
+    /// The <see cref="HttpListener"/> instance associated with this
+    /// collection is closed.
     /// </exception>
     public void CopyTo (string[] array, int offset)
     {
