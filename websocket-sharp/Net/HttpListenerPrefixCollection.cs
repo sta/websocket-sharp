70852b1267cfff4393563948634f1d1579a9615c
sta <sta.blockhead@gmail.com>
2020-09-03 21:39:33 +0900

    [Modify] Edit it

--- websocket-sharp/Net/HttpListenerPrefixCollection.cs
+++ websocket-sharp/Net/HttpListenerPrefixCollection.cs
@@ -133,8 +133,8 @@ namespace WebSocketSharp.Net
     /// <paramref name="uriPrefix"/> is invalid.
     /// </exception>
     /// <exception cref="ObjectDisposedException">
-    /// The <see cref="HttpListener"/> instance associated with this collection
-    /// is closed.
+    /// The <see cref="HttpListener"/> instance associated with this
+    /// collection is closed.
     /// </exception>
     public void Add (string uriPrefix)
     {
