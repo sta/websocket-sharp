40a78fb1ccc8fd877ee9d1d38e0d805c288c0078
sta <sta.blockhead@gmail.com>
2020-09-26 21:14:48 +0900

    [Modify] Replace it

--- websocket-sharp/Net/EndPointManager.cs
+++ websocket-sharp/Net/EndPointManager.cs
@@ -139,7 +139,7 @@ namespace WebSocketSharp.Net
 
     private static void removePrefix (string uriPrefix, HttpListener listener)
     {
-      var pref = new HttpListenerPrefix (uriPrefix);
+      var pref = new HttpListenerPrefix (uriPrefix, listener);
 
       var addr = convertToIPAddress (pref.Host);
 
