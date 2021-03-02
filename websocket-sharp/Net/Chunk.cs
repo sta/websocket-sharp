f6c5c70cd9bcbb9e02bc9006f1de9c9c28b228fb
sta <sta.blockhead@gmail.com>
2021-03-02 19:40:21 +0900

    [Modify] Polish it

--- websocket-sharp/Net/Chunk.cs
+++ websocket-sharp/Net/Chunk.cs
@@ -74,13 +74,15 @@ namespace WebSocketSharp.Net
     public int Read (byte[] buffer, int offset, int count)
     {
       var left = _data.Length - _offset;
+
       if (left == 0)
-        return left;
+        return 0;
 
       if (count > left)
         count = left;
 
       Buffer.BlockCopy (_data, _offset, buffer, offset, count);
+
       _offset += count;
 
       return count;
