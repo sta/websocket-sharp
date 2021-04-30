b30e5e52fea7c484c85a9e26d573ae6f6d503ca1
sta <sta.blockhead@gmail.com>
2021-04-30 20:37:15 +0900

    [Modify] Replace it

--- websocket-sharp/Net/HttpListener.cs
+++ websocket-sharp/Net/HttpListener.cs
@@ -250,7 +250,9 @@ namespace WebSocketSharp.Net
       }
 
       set {
-        CheckDisposed ();
+        if (_disposed)
+          throw new ObjectDisposedException (_objectName);
+
         _certFolderPath = value;
       }
     }
