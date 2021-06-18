ca85c785a3d202900c1ea70dc0dc762fc2feb2ae
sta <sta.blockhead@gmail.com>
2021-06-18 19:49:43 +0900

    [Modify] Polish it

--- websocket-sharp/Server/HttpServer.cs
+++ websocket-sharp/Server/HttpServer.cs
@@ -625,7 +625,7 @@ namespace WebSocketSharp.Server
     public ServerSslConfiguration SslConfiguration {
       get {
         if (!_secure) {
-          var msg = "This instance does not provide secure connections.";
+          var msg = "The server does not provide secure connections.";
 
           throw new InvalidOperationException (msg);
         }
