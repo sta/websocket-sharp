e80a7e844ca4eb5fd8a01487fbb94aba9c25f45e
sta <sta.blockhead@gmail.com>
2021-07-28 19:58:35 +0900

    [Modify] Edit it

--- websocket-sharp/Server/HttpRequestEventArgs.cs
+++ websocket-sharp/Server/HttpRequestEventArgs.cs
@@ -156,8 +156,8 @@ namespace WebSocketSharp.Server
     #region Public Methods
 
     /// <summary>
-    /// Reads the specified file from the document folder of
-    /// the <see cref="HttpServer"/>.
+    /// Reads the specified file from the document folder of the
+    /// <see cref="HttpServer"/> class.
     /// </summary>
     /// <returns>
     ///   <para>
