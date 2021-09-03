2c87161976d89ace0fa50d764ca7d7d98841ee27
sta <sta.blockhead@gmail.com>
2021-09-03 19:45:42 +0900

    [Modify] Edit it

--- websocket-sharp/Server/WebSocketServer.cs
+++ websocket-sharp/Server/WebSocketServer.cs
@@ -1076,7 +1076,7 @@ namespace WebSocketSharp.Server
     /// </summary>
     /// <param name="path">
     ///   <para>
-    ///   A <see cref="string"/> that represents an absolute path to
+    ///   A <see cref="string"/> that specifies an absolute path to
     ///   the service to add.
     ///   </para>
     ///   <para>
