c53c6c99c3103770b9cd049ac6df49ef1992757e
sta <sta.blockhead@gmail.com>
2021-09-13 20:21:37 +0900

    [Modify] Edit it

--- websocket-sharp/Server/WebSocketServer.cs
+++ websocket-sharp/Server/WebSocketServer.cs
@@ -323,8 +323,8 @@ namespace WebSocketSharp.Server
     /// Gets the IP address of the server.
     /// </summary>
     /// <value>
-    /// A <see cref="System.Net.IPAddress"/> that represents the local
-    /// IP address on which to listen for incoming handshake requests.
+    /// A <see cref="System.Net.IPAddress"/> that represents the local IP
+    /// address on which to listen for incoming handshake requests.
     /// </value>
     public System.Net.IPAddress Address {
       get {
