711672203266f33f0510baec6f25b9e36ab706b6
sta <sta.blockhead@gmail.com>
2021-09-16 19:29:34 +0900

    [Modify] Edit it

--- websocket-sharp/Server/HttpServer.cs
+++ websocket-sharp/Server/HttpServer.cs
@@ -724,12 +724,12 @@ namespace WebSocketSharp.Server
     }
 
     /// <summary>
-    /// Gets the management function for the WebSocket services
-    /// provided by the server.
+    /// Gets the management function for the WebSocket services provided by
+    /// the server.
     /// </summary>
     /// <value>
-    /// A <see cref="WebSocketServiceManager"/> that manages
-    /// the WebSocket services provided by the server.
+    /// A <see cref="WebSocketServiceManager"/> that manages the WebSocket
+    /// services provided by the server.
     /// </value>
     public WebSocketServiceManager WebSocketServices {
       get {
