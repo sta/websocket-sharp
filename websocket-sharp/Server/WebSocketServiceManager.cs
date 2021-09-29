f4241368a8fc1fbd9bc1285ff924a19f91f5191b
sta <sta.blockhead@gmail.com>
2021-09-29 19:14:26 +0900

    [Modify] Remove it

--- websocket-sharp/Server/WebSocketServiceManager.cs
+++ websocket-sharp/Server/WebSocketServiceManager.cs
@@ -555,33 +555,6 @@ namespace WebSocketSharp.Server
     }
 
     /// <summary>
-    /// Sends a ping to every client in the WebSocket services.
-    /// </summary>
-    /// <returns>
-    ///   <para>
-    ///   A <c>Dictionary&lt;string, Dictionary&lt;string, bool&gt;&gt;</c>.
-    ///   </para>
-    ///   <para>
-    ///   It represents a collection of pairs of a service path and another
-    ///   collection of pairs of a session ID and a value indicating whether
-    ///   a pong has been received from the client within a time.
-    ///   </para>
-    /// </returns>
-    /// <exception cref="InvalidOperationException">
-    /// The current state of the manager is not Start.
-    /// </exception>
-    [Obsolete ("This method will be removed.")]
-    public Dictionary<string, Dictionary<string, bool>> Broadping ()
-    {
-      if (_state != ServerState.Start) {
-        var msg = "The current state of the manager is not Start.";
-        throw new InvalidOperationException (msg);
-      }
-
-      return broadping (WebSocketFrame.EmptyPingBytes, _waitTime);
-    }
-
-    /// <summary>
     /// Sends a ping with <paramref name="message"/> to every client in
     /// the WebSocket services.
     /// </summary>
