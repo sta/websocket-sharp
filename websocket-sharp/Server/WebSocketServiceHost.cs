55f01e43bedb2363fa447f44938a41514a35516e
sta <sta.blockhead@gmail.com>
2021-10-12 19:28:56 +0900

    [Modify] Edit it

--- websocket-sharp/Server/WebSocketServiceHost.cs
+++ websocket-sharp/Server/WebSocketServiceHost.cs
@@ -164,8 +164,8 @@ namespace WebSocketSharp.Server
     public abstract Type BehaviorType { get; }
 
     /// <summary>
-    /// Gets or sets the time to wait for the response to the WebSocket Ping or
-    /// Close.
+    /// Gets or sets the time to wait for the response to the WebSocket Ping
+    /// or Close.
     /// </summary>
     /// <remarks>
     /// The set operation does nothing if the service has already started or
