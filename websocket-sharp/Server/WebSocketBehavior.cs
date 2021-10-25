d45eaf01e182170eb5e99733597717499c17c42e
sta <sta.blockhead@gmail.com>
2021-10-25 21:10:18 +0900

    [Modify] Polish it

--- websocket-sharp/Server/WebSocketBehavior.cs
+++ websocket-sharp/Server/WebSocketBehavior.cs
@@ -926,6 +926,7 @@ namespace WebSocketSharp.Server
     {
       if (_websocket == null) {
         var msg = "The current state of the connection is not Open.";
+
         throw new InvalidOperationException (msg);
       }
 
