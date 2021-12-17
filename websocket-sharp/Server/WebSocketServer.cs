f93ec070511c2f5ce20a56fa9491236e99465f92
sta <sta.blockhead@gmail.com>
2021-12-17 19:53:37 +0900

    [Modify] Replace it

--- websocket-sharp/Server/WebSocketServer.cs
+++ websocket-sharp/Server/WebSocketServer.cs
@@ -391,13 +391,8 @@ namespace WebSocketSharp.Server
 
       set {
         lock (_sync) {
-          string msg;
-
-          if (!canSet (out msg)) {
-            _log.Warn (msg);
-
+          if (!canSet ())
             return;
-          }
 
           _authSchemes = value;
         }
