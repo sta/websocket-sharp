8ab45316d632c75d54c58135b2acda7947ae5368
sta <sta.blockhead@gmail.com>
2022-01-01 17:31:46 +0900

    [Modify] Replace it

--- websocket-sharp/Server/HttpServer.cs
+++ websocket-sharp/Server/HttpServer.cs
@@ -553,13 +553,8 @@ namespace WebSocketSharp.Server
 
       set {
         lock (_sync) {
-          string msg;
-
-          if (!canSet (out msg)) {
-            _log.Warn (msg);
-
+          if (!canSet ())
             return;
-          }
 
           _listener.Realm = value;
         }
