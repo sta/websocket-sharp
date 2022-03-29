a2f287474df8ae08c3689874eadf134394c77c64
sta <sta.blockhead@gmail.com>
2022-03-29 17:03:15 +0900

    [Modify] Edit it

--- websocket-sharp/Net/HttpListenerRequest.cs
+++ websocket-sharp/Net/HttpListenerRequest.cs
@@ -369,8 +369,8 @@ namespace WebSocketSharp.Net
     /// Gets the endpoint to which the request is sent.
     /// </summary>
     /// <value>
-    /// A <see cref="System.Net.IPEndPoint"/> that represents the server IP
-    /// address and port number.
+    /// A <see cref="System.Net.IPEndPoint"/> that represents the server
+    /// IP address and port number.
     /// </value>
     public System.Net.IPEndPoint LocalEndPoint {
       get {
