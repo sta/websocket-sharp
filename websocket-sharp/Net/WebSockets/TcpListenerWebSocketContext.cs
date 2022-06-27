b2a183d54fe2d53eb972245400e1d3fd8e2d8489
sta <sta.blockhead@gmail.com>
2022-06-27 20:28:51 +0900

    [Modify] Edit it

--- websocket-sharp/Net/WebSockets/TcpListenerWebSocketContext.cs
+++ websocket-sharp/Net/WebSockets/TcpListenerWebSocketContext.cs
@@ -413,8 +413,8 @@ namespace WebSocketSharp.Net.WebSockets
     /// Gets the endpoint from which the handshake request is sent.
     /// </summary>
     /// <value>
-    /// A <see cref="System.Net.IPEndPoint"/> that represents the client IP
-    /// address and port number.
+    /// A <see cref="System.Net.IPEndPoint"/> that represents the client
+    /// IP address and port number.
     /// </value>
     public override System.Net.IPEndPoint UserEndPoint {
       get {
