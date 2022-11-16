c5ad157f3083efeb324c1db8e7cb66cc707c98a1
sta <sta.blockhead@gmail.com>
2022-11-16 16:03:36 +0900

    [Modify] Polish it

--- websocket-sharp/Server/WebSocketBehavior.cs
+++ websocket-sharp/Server/WebSocketBehavior.cs
@@ -1045,7 +1045,7 @@ namespace WebSocketSharp.Server
     protected void Send (string data)
     {
       if (_websocket == null) {
-        var msg = "The current state of the connection is not Open.";
+        var msg = "The session has not started yet.";
 
         throw new InvalidOperationException (msg);
       }
