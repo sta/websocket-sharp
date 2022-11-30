4191a3c3634bf4ff1ab78cc584e7b64000aa73d6
sta <sta.blockhead@gmail.com>
2022-11-30 15:57:10 +0900

    [Modify] Polish it

--- websocket-sharp/WebSocket.cs
+++ websocket-sharp/WebSocket.cs
@@ -3262,7 +3262,7 @@ namespace WebSocketSharp
     public void ConnectAsync ()
     {
       if (!_client) {
-        var msg = "The instance is not a client.";
+        var msg = "The interface is not for the client.";
 
         throw new InvalidOperationException (msg);
       }
