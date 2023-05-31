18974e3f5124c1468fc9e21b6d2f0710a39bcc29
sharp00k <sharp00k@gmail.com>
2023-05-31 17:18:09 +0900

    add_http_response_code_and_body_when_handshake_error

--- websocket-sharp/PayloadData.cs
+++ websocket-sharp/PayloadData.cs
@@ -95,11 +95,24 @@ namespace WebSocketSharp
       _data = code.Append (reason);
       _length = _data.LongLength;
     }
+    
+    internal PayloadData (ushort code, string reason, int httpStatusCode, string httpResponseBody)
+    {
+      _data = code.Append (reason);
+      _length = _data.LongLength;
+      HttpStatusCode = httpStatusCode;
+      HttpResponseBody = httpResponseBody;
+
+    }
 
     #endregion
 
     #region Internal Properties
 
+    internal int HttpStatusCode { get; }
+    
+    internal string HttpResponseBody { get; }
+    
     internal ushort Code {
       get {
         return _length >= 2
