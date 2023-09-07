0db20685cd2564ca21e29623fd35024cc25c03eb
sta <sta.blockhead@gmail.com>
2023-09-07 16:17:51 +0900

    [Modify] Polish it

--- websocket-sharp/Net/HttpListenerResponse.cs
+++ websocket-sharp/Net/HttpListenerResponse.cs
@@ -297,16 +297,19 @@ namespace WebSocketSharp.Net
       set {
         if (_disposed) {
           var name = GetType ().ToString ();
+
           throw new ObjectDisposedException (name);
         }
 
         if (_headersSent) {
           var msg = "The response is already being sent.";
+
           throw new InvalidOperationException (msg);
         }
 
         if (value < 0) {
           var msg = "Less than zero.";
+
           throw new ArgumentOutOfRangeException (msg, "value");
         }
 
