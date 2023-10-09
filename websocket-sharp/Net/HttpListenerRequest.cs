2d9e6e646357b15f78087497547b40c801989a47
sta <sta.blockhead@gmail.com>
2023-10-09 16:36:06 +0900

    [Modify] Edit it

--- websocket-sharp/Net/HttpListenerRequest.cs
+++ websocket-sharp/Net/HttpListenerRequest.cs
@@ -459,10 +459,7 @@ namespace WebSocketSharp.Net
     /// </summary>
     /// <value>
     ///   <para>
-    ///   A <see cref="Uri"/> or <see langword="null"/>.
-    ///   </para>
-    ///   <para>
-    ///   The Uri represents the URL parsed from the request.
+    ///   A <see cref="Uri"/> that represents the URL parsed from the request.
     ///   </para>
     ///   <para>
     ///   <see langword="null"/> if the URL cannot be parsed.
