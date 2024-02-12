34b3a13f3d91989b1d35556bcae35f0d50abcd7d
sta <sta.blockhead@gmail.com>
2024-02-12 21:42:53 +0900

    [Modify] Replace it

--- websocket-sharp/Net/HttpListener.cs
+++ websocket-sharp/Net/HttpListener.cs
@@ -404,7 +404,7 @@ namespace WebSocketSharp.Net
 
       set {
         if (_disposed)
-          throw new ObjectDisposedException (_objectName);
+          throw new ObjectDisposedException (ObjectName);
 
         _realm = value;
       }
