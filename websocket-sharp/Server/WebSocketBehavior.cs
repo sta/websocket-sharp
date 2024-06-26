79e751cb10432ca9100501c0491a6cba361c5037
sta <sta.blockhead@gmail.com>
2024-06-26 20:41:43 +0900

    [Modify] Edit it

--- websocket-sharp/Server/WebSocketBehavior.cs
+++ websocket-sharp/Server/WebSocketBehavior.cs
@@ -882,9 +882,6 @@ namespace WebSocketSharp.Server
     ///   Its size must be 123 bytes or less in UTF-8.
     ///   </para>
     /// </param>
-    /// <exception cref="InvalidOperationException">
-    /// The session has not started yet.
-    /// </exception>
     /// <exception cref="ArgumentException">
     ///   <para>
     ///   <paramref name="code"/> is <see cref="CloseStatusCode.MandatoryExtension"/>.
@@ -905,6 +902,9 @@ namespace WebSocketSharp.Server
     /// </exception>
     /// <exception cref="ArgumentOutOfRangeException">
     /// The size of <paramref name="reason"/> is greater than 123 bytes.
+    /// </exception>
+    /// <exception cref="InvalidOperationException">
+    /// The session has not started yet.
     /// </exception>
     protected void CloseAsync (CloseStatusCode code, string reason)
     {
