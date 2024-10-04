a2d492f0a332373d634bf6f1986c70738fd4eb44
sta <sta.blockhead@gmail.com>
2024-10-04 21:29:40 +0900

    [Modify] Polish it

--- websocket-sharp/Net/AuthenticationResponse.cs
+++ websocket-sharp/Net/AuthenticationResponse.cs
@@ -196,7 +196,9 @@ namespace WebSocketSharp.Net
     #region Private Methods
 
     private static string createA1 (
-      string username, string password, string realm
+      string username,
+      string password,
+      string realm
     )
     {
       return String.Format ("{0}:{1}:{2}", username, realm, password);
