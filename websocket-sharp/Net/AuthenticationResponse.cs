2debbf2f16a4ba94fa135a7fef1262f9524adeb8
sta <sta.blockhead@gmail.com>
2024-10-13 21:11:53 +0900

    [Modify] Polish it

--- websocket-sharp/Net/AuthenticationResponse.cs
+++ websocket-sharp/Net/AuthenticationResponse.cs
@@ -271,7 +271,7 @@ namespace WebSocketSharp.Net
 
     internal static string CreateRequestDigest (NameValueCollection parameters)
     {
-      var user = parameters["username"];
+      var uname = parameters["username"];
       var pass = parameters["password"];
       var realm = parameters["realm"];
       var nonce = parameters["nonce"];
@@ -283,8 +283,8 @@ namespace WebSocketSharp.Net
       var method = parameters["method"];
 
       var a1 = algo != null && algo.ToLower () == "md5-sess"
-               ? createA1 (user, pass, realm, nonce, cnonce)
-               : createA1 (user, pass, realm);
+               ? createA1 (uname, pass, realm, nonce, cnonce)
+               : createA1 (uname, pass, realm);
 
       var a2 = qop != null && qop.ToLower () == "auth-int"
                ? createA2 (method, uri, parameters["entity"])
