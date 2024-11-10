9f84131fc0f2871488f0f922668ea9e95c81c677
sta <sta.blockhead@gmail.com>
2024-11-10 21:13:43 +0900

    [Modify] 2024

--- websocket-sharp/Net/CookieException.cs
+++ websocket-sharp/Net/CookieException.cs
@@ -7,7 +7,7 @@
  *
  * The MIT License
  *
- * Copyright (c) 2012-2019 sta.blockhead
+ * Copyright (c) 2012-2024 sta.blockhead
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
