d6a463cad493e71d4d241dabc0bc42299eaa5991
sta <sta.blockhead@gmail.com>
2025-04-10 16:26:20 +0900

    [Modify] Polish it

--- websocket-sharp/Server/WebSocketBehavior.cs
+++ websocket-sharp/Server/WebSocketBehavior.cs
@@ -941,7 +941,7 @@ namespace WebSocketSharp.Server
     protected void CloseAsync (ushort code, string reason)
     {
       if (_websocket == null) {
-        var msg = "The session has not started yet.";
+        var msg = "The CloseAsync method is not available.";
 
         throw new InvalidOperationException (msg);
       }
