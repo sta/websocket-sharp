038e52d5bb84932f372b2c01560e186964ebd130
sta <sta.blockhead@gmail.com>
2025-04-15 16:40:17 +0900

    [Modify] Edit it

--- websocket-sharp/Server/WebSocketBehavior.cs
+++ websocket-sharp/Server/WebSocketBehavior.cs
@@ -1175,7 +1175,8 @@ namespace WebSocketSharp.Server
     /// </exception>
     /// <exception cref="InvalidOperationException">
     ///   <para>
-    ///   The Send method is not available when the session has not started yet.
+    ///   The Send method is not available when the session has not
+    ///   started yet.
     ///   </para>
     ///   <para>
     ///   -or-
