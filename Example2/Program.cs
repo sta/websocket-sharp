da2c1aafb904bc57855c0f0942443eee8fcbe4d4
sta <sta.blockhead@gmail.com>
2025-04-27 16:32:20 +0900

    [Modify] Edit it

--- Example2/Program.cs
+++ Example2/Program.cs
@@ -14,7 +14,7 @@ namespace Example2
       // Create a new instance of the WebSocketServer class.
       //
       // If you would like to provide the secure connection, you should
-      // create a new instance with the 'secure' parameter set to true or
+      // create a new instance with the "secure" parameter set to true or
       // with a wss scheme WebSocket URL.
 
       var wssv = new WebSocketServer (4649);
