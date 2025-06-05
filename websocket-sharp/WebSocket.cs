ebd7a97745d765ccc6f4c0278a7b84e057fc48c2
sta <sta.blockhead@gmail.com>
2025-06-05 16:15:35 +0900

    [Modify] Edit it

--- websocket-sharp/WebSocket.cs
+++ websocket-sharp/WebSocket.cs
@@ -3734,7 +3734,7 @@ namespace WebSocketSharp
     ///   An <see cref="T:System.Action{bool}"/> delegate.
     ///   </para>
     ///   <para>
-    ///   The delegate invokes the method called when the send is complete.
+    ///   It specifies the delegate called when the send is complete.
     ///   </para>
     ///   <para>
     ///   The <see cref="bool"/> parameter passed to the method is <c>true</c>
