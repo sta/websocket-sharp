49dda162dfd5f28b119cd6fee88c3a81416e3431
sta <sta.blockhead@gmail.com>
2025-06-06 16:29:18 +0900

    [Modify] Polish it

--- websocket-sharp/WebSocket.cs
+++ websocket-sharp/WebSocket.cs
@@ -3814,7 +3814,7 @@ namespace WebSocketSharp
     public void SendAsync (FileInfo fileInfo, Action<bool> completed)
     {
       if (_readyState != WebSocketState.Open) {
-        var msg = "The current state of the interface is not Open.";
+        var msg = "The SendAsync method is not available.";
 
         throw new InvalidOperationException (msg);
       }
